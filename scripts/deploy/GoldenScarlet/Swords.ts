55beb8706b9e62a0aa12c97b824eb9ff8d29b1bc
LeonardsonCC <leonardson.carvalho@gmail.com>
2022-02-26 13:32:06 -0300

    refactor: scripts location

--- scripts/deploy/GoldenScarlet/Swords.ts
+++ scripts/deploy/GoldenScarlet/Swords.ts
@@ -1,9 +1,10 @@
-import { ethers } from "hardhat";
+import {ethers} from "hardhat";
 
 async function main() {
   const Swords = await ethers.getContractFactory("GoldenScarletSwords");
   const swords = await Swords.deploy([
     "0x70b70E0658F81d56C30E43b71be3922Ec4aF213E",
+    "0xf7159D1E34EF333aB7737607d96d1F30E72a1Bb9",
   ]);
 
   await swords.deployed();
